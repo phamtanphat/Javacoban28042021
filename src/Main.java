4d151859ac767e207e397d4ef2487e12eec24a3b
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-05-05 20:34:21 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // 1 : Kiểu dữ liệu
+//        String , int , long , float , double , boolean , char
+//        String name = "Phạm Tấn Phát";
+//        int age = 10; // Số nguyên
+//        float weight = 1.2f; // Số thực
+//        double weight2 = 1.2;
+//        boolean isLogin = false;
+//         google dich sang tiếng anh
+//         camelcase : quy tắc đặt tên biến
+//        comment code : không sử dụng ctrl + /
+//         2 : Câu điều kiện
+//
+//
+//         a > b = 1;
+//         a == b = 0;
+//         a < b = -1;
+//
+//         Scope {} = chứa đoạn lệnh
+//         () = chứa giá trị
+//        int a = 15;
+//        int b = 10;
+//        if (a < b) {
+//            System.out.print("-1");
+//        }else if (a == b){
+//            System.out.print("0");
+//        } else{
+//            System.out.print("1");
+//        }
+//        3 : Toán tử
+//        int a = 5;
+//        int b = 5;
+//         Thu 1 : lay gia tri cua a gan b
+//         Thu 2 : a = a + 1
+//        System.out.println(b);
+//        System.out.println(a);
+//        int ketQua = ++a - --b + b-- - b++ + a-- - --a + b--;
+//        6 - --b + b-- - b++ + a-- - --a + b--    a= 6 , b = 5
+//        6 - 4 + b-- - b++ + a-- - --a + b--      a= 6 , b = 4
+//        6 - 4 + 4 - b++ + a-- - --a + b--      a= 6 , b = 3
+//        6 - 4 + 4 - 3 + a-- - --a + b--      a= 6 , b = 4
+//        6 - 4 + 4 - 3 + 6 - --a + b--      a= 5 , b = 4
+//        6 - 4 + 4 - 3 + 6 - 4 + b--      a= 4 , b = 4
+//        6 - 4 + 4 - 3 + 6 - 4 + 4      a= 4 , b = 3
+//        3 + 6
+//        9
+//        System.out.print(ketQua);
+
+
+        // 3 , 7  ,17
+
+    }
+}
